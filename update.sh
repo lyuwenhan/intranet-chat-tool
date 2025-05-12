32e1f59ea8ae7fe38000c360b403bb53a6142ac3
lyuwenhan <lyuwenhan@gmail.com>
2025-05-12 19:23:11 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -14,6 +14,8 @@ if ! git diff --quiet HEAD origin/main; then
     npm install --no-audit --no-fund --prefer-offline
     chmod u+x *.sh
     g++ judge/judge.cpp -o judge/judge.out -O2
+    exit 0
 else
     echo "[Git] Already up-to-date."
+    exit 1
 fi
