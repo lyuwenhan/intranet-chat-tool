5632650f3813fdaa360e6463cfce337e9e042b56
lyuwenhan <lyuwenhan@gmail.com>
2025-05-16 19:35:36 -0400

    Update node.js

--- node.js
+++ node.js
@@ -2077,7 +2077,7 @@ function runcpp(command, cpp, input, output, errfile, callback, token){
 		fs.writeFileSync(`${userFileDir}/user.in`, input);
 		let result;
 		try{
-			result = await start_runcpp(command, ["run", "--rm", "--network", "none", "--read-only", "--pids-limit=64", "--cpus=0.5", "--memory=130m", "--cap-drop=ALL", "--security-opt", "no-new-privileges", "--tmpfs", "/tmp", "-v", `${path.resolve(userFileDir)}:${userFileDirInDocker}:rw`, "judge-runner", `${userFileDirInDocker}/user.cpp`, `${userFileDirInDocker}/user.in`, `${userFileDirInDocker}/user.out`, `${userFileDirInDocker}/user.err`, `/app/user.run`, String(timeout), "128", "1048576", "-O2"]);
+			result = await start_runcpp(command, ["run", "--rm", "--network", "none", "--read-only", "--pids-limit=64", "--cpus=0.5", "--memory=130m", "--cap-drop=ALL", "--security-opt", "no-new-privileges", "--tmpfs", "/tmp", "--tmpfs", "/app", "-v", `${path.resolve(userFileDir)}:${userFileDirInDocker}:rw`, "judge-runner", `${userFileDirInDocker}/user.cpp`, `${userFileDirInDocker}/user.in`, `${userFileDirInDocker}/user.out`, `${userFileDirInDocker}/user.err`, `/app/user.run`, String(timeout), "128", "1048576", "-O2"]);
 		}finally{
 			try{
 				if(fs.existsSync(`${userFileDir}/user.out`)){
