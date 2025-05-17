012fe53c162fd6edf06f155db9344a7b9a8eed9d
lyuwenhan <lyuwenhan@gmail.com>
2025-05-17 12:46:37 -0400

    merge update.sh and updatef.sh by checking -f

--- update.sh
+++ update.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 set -euo pipefail
-cd "$(dirname "$0")"  # 切换到脚本所在目录，避免 cron 中路径错乱
+cd "$(dirname "$0")"
 
-echo "[Git] Checking for updates at $(date)"
-
-git fetch origin main
-
-if ! git diff --quiet HEAD origin/main; then
-    echo "[Git] Remote has new commits. Pulling..."
-    git reset --hard HEAD
-    git pull --rebase --autostash origin main
-    echo "[Git] Running npm install..."
-    npm install --no-audit --no-fund --prefer-offline
-    chmod u+x ./*.sh
-    chmod g+x ./*.sh
-    g++ judge/judge.cpp -o judge/judge.out -O2
-    exit 0
+if [[ " $* " == *" -f "* ]]; then
+    echo "[Git] Checking for updates at $(date)"
+    git fetch origin main
+    if ! git diff --quiet HEAD origin/main; then
+        echo "[Git] Remote has new commits. Pulling..."
+        git reset --hard HEAD
+        git pull --rebase --autostash origin main
+    else
+        echo "[Git] Already up-to-date."
+        exit 1
+    fi
 else
-    echo "[Git] Already up-to-date."
-    exit 1
+    git fetch origin
+    git reset --hard origin/main
 fi
+echo "[Git] Running npm install..."
+npm install --no-audit --no-fund --prefer-offline
+chmod u+x ./*.sh
+chmod g+x ./*.sh
+g++ judge/judge.cpp -o judge/judge.out -O2
